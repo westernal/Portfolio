3ffc370c46975fbe0c98351ca24c842505f58658
ali navidi <ali_navidi24@yahoo.com>
2022-07-16 20:29:19 +0430

    about me changed!

--- components/about.jsx
+++ components/about.jsx
@@ -4,8 +4,12 @@ const About = () => {
   return (
     <div className="about" id="about">
       <h1>Ali Navidi</h1>
-      <p>Frontend Developer</p>
-      <SocialIcons />
+      <h2>Frontend Developer</h2>
+      <p>
+        Hi, I'm a 21-year-old Frontend developer but I have some backend
+        knowledge and I'm currently studying my last year of software
+        engineering.
+      </p>
       <a href="/My Resume.pdf" download={true}>
         <div className="btn from-top resume">Resume</div>
       </a>
