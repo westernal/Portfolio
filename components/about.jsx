233696c92e77622fa62f7f8252f44dee4a35a119
ali navidi <ali_navidi24@yahoo.com>
2022-08-18 01:01:40 +0430

    about changed

--- components/about.jsx
+++ components/about.jsx
@@ -1,16 +1,16 @@
-import SocialIcons from "./socialIcons";
+import Image from "next/image";
 
 const About = () => {
   return (
     <div className="about" id="about">
       <h1>Ali Navidi</h1>
 
-      <a href="https://git.io/typing-svg">
-        <img
-          src="https://readme-typing-svg.herokuapp.com?font=Roboto+Condensed&size=28&pause=1000&color=FFFFFF&center=true&vCenter=true&width=435&lines=Full-stack+web+developer;Software+engineering+student;Always+learning+new+things;Working+hard+to+be+better"
-          alt="Typing SVG"
-        />
-      </a>
+      <Image
+        src="/Images/readme-typing-svg_herokuapp_default.svg"
+        alt="Typing SVG"
+        width={435}
+        height={50}
+      />
 
       <a href="/My Resume.pdf" download={true}>
         <div className="btn from-top resume">Resume</div>
