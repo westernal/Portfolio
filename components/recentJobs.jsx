b3fccf8a7ef7d7b5cca0e2007a380d4e017b84bc
ali navidi <ali_navidi24@yahoo.com>
2022-08-29 23:07:51 +0430

    new job added

--- components/recentJobs.jsx
+++ components/recentJobs.jsx
@@ -4,6 +4,7 @@ const Jobs = () => {
   return (
     <div className="jobs reveal" id="jobs">
       <h2>Recent Jobs</h2>
+
       <div className="job">
         <div className="job-profile">
           <Image
@@ -14,10 +15,25 @@ const Jobs = () => {
           />
           <div className="job-name">
             <h3>Front-End Developer</h3>
-            <p>Oben Origin co.</p>
+            <p>Oben Origin co. - Tehran, Iran</p>
           </div>
         </div>
-        <div className="date">2020 - 2021</div>
+        <div className="date">01/2020 - 11/2021</div>
+      </div>
+      <div className="job">
+        <div className="job-profile">
+          <Image
+            width={100}
+            height={100}
+            src="/Images/restarone.svg"
+            alt="oben origin"
+          />
+          <div className="job-name">
+            <h3>Web Developer</h3>
+            <p>Restarone Solutions Inc. - Toronto, Canada</p>
+          </div>
+        </div>
+        <div className="date">09/2022 - Now</div>
       </div>
     </div>
   );
