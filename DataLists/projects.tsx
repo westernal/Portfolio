637489026539513e6168abf706d3f158d7eb6b83
ali navidi <ali_navidi24@yahoo.com>
2023-01-30 18:28:51 +0330

    bugs fixed

--- DataLists/projects.tsx
+++ DataLists/projects.tsx
@@ -10,7 +10,7 @@ export const projects: Project[] = [
       designer: true,
     },
     description:
-      "Westernal is a social media based on songs, you can share posts that includes part of a song, the whole song or music videos with your followers and let the music talk for you! I made it using Next.js for frontend, Node.js and Express.js for backend, MongoDB for database and Cypress for testing.",
+      "Westernal is a social media based on songs, you can share posts that includes part of a song, the whole song or music videos with your followers and let the music talk for you! made with Next.js for frontend, Node.js and Express.js for backend, MongoDB for database and Cypress for testing.",
     image: {
       mobile: "/Images/mobile.png",
     },
@@ -31,7 +31,7 @@ export const projects: Project[] = [
       designer: true,
     },
     description:
-      "Cinema is a platform for watching and downloading movies. I made it using Next.js.",
+      "Cinema is a platform for watching and downloading movies. made with Next.js.",
     image: {
       mobile:
         "/Images/5279C0FE-F244-4B54-B9C6-BB18F7D07078_iphone13blue_portrait.png",
@@ -50,7 +50,7 @@ export const projects: Project[] = [
       designer: true,
     },
     description:
-      "Sartel is a website selling mobile phones and accessories. I made it using Next.js and TypeScript.",
+      "Sartel is a website selling mobile phones and accessories. made with Next.js and TypeScript.",
     image: {
       mobile:
         "/Images/DB43DA89_2874_487D_A50A_CBE7E281960F_iphone13starlight_portrait.png",
@@ -69,7 +69,7 @@ export const projects: Project[] = [
       backEnd: false,
       designer: true,
     },
-    description: "An admin dashboard made by Next.js.",
+    description: "An admin dashboard made with Next.js.",
     image: {
       mobile:
         "/Images/A05D135D-6A5D-4210-AFB3-C35D4F73284F_iphone13red_portrait.png",
