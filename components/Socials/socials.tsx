6caa915125bd97c168094242133f79134b420379
ali navidi <ali_navidi24@yahoo.com>
2023-02-08 14:45:20 +0330

    bugs fixed

--- components/Socials/socials.tsx
+++ components/Socials/socials.tsx
@@ -4,9 +4,6 @@ const Socials = () => {
   return (
     <section className="socials reveal" id="socials">
       <h2>Follow me everywhere!</h2>
-      <p id="song">
-        Cause you{"'"}re a sky, you{"'"}re a sky full of stars
-      </p>
       <SocialIcons />
       <a href="mailto:ali_navidi24@yahoo.com" className="btn from-top">
         Email me
