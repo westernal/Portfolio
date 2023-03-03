bf039d55df811dbd480b6692244fb5efbfbb6237
ali navidi <ali_navidi24@yahoo.com>
2023-03-03 16:31:57 +0330

    bugs fixed

--- components/Layout/Effects/shootingStars.tsx
+++ components/Layout/Effects/shootingStars.tsx
@@ -2,9 +2,6 @@ const ShootingStars = () => {
   return (
     <div className="shooting-stars">
       <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
     </div>
   );
 };
