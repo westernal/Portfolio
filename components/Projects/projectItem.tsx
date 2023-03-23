c6f1994f429b92e5f44a7b2fc66411085b95929d
ali navidi <ali_navidi24@yahoo.com>
2023-03-23 19:27:34 +0430

    Bugs fixed

--- components/Projects/projectItem.tsx
+++ components/Projects/projectItem.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { Project } from "../../Interfaces/Interfaces";
 
 const ProjectItem = ({ project }: { project: Project }) => {
