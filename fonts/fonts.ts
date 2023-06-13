e8b35874f4d47fc2f0fff3ebf72408042282b4aa
ali navidi <ali_navidi24@yahoo.com>
2023-06-14 00:39:02 +0330

    Fonts optimized

---
+++ fonts/fonts.ts
@@ -0,0 +1,15 @@
+import {Orbitron, Montserrat} from "next/font/google"
+
+export const montserrat = Montserrat({
+    variable: "--font-main",
+    weight: "400",
+    display: "swap",
+    subsets: ["latin"],
+  });
+  
+  export const orbitron = Orbitron({
+    variable: "--font-logo",
+    weight: "400",
+    display: "swap",
+    subsets: ["latin"],
+  });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import { montserrat, orbitron } from "../fonts/fonts";
 import "../styles/style.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${montserrat.variable} ${orbitron.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
