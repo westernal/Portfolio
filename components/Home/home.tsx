2209e56a6b219cfd193020eef8e630c9e30557ed
ali navidi <ali_navidi24@yahoo.com>
2023-07-08 16:31:40 +0330

    Bugs fixed

--- components/Home/home.tsx
+++ components/Home/home.tsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <section className="about home center active-page" id="about">
+    <section className="about center active-page" id="home">
       <section className="info ">
         <div className="flex name">
           <span className="first-name">
