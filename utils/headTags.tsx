c53ad1c1780cc6dc209003fa4caa5d21b6843efd
ali navidi <ali_navidi24@yahoo.com>
2024-07-17 16:42:41 +0330

    Viewport tag changed

--- utils/headTags.tsx
+++ utils/headTags.tsx
@@ -5,10 +5,7 @@ const HeadTags = () => {
     <Head>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Ali Navidi | Frontend Developer</title>
       <meta
         name="description"
