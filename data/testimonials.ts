56134623f7b52c8b1c39897f4c6a5f950eed0d97
ali navidi <ali_navidi24@yahoo.com>
2024-08-12 13:16:07 +0330

    New testimonial added

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -25,12 +25,12 @@ export const testimonials: Testimonial[] = [
       "Any company lucky enough to discover him will be getting a potential team leader. Generous with his knowledge and selfless in his efforts for his team. It was a pleasure to work alongside him, and I hope to work with him again because he is a great developer and person.",
     role: "Frontend Developer",
   },
-  // {
-  //   id: 2,
-  //   name: "Gabrielle Callegari",
-  //   avatar: "/Images/Calle.jpg",
-  //   description:
-  //     "Working alongside Ali was a privilege. I can confidently say that he's an outstanding frontend developer and leader. Throughout our time working together, I've consistently admired Ali's technical expertise, as well as his ability to connect with others on a personal level. With his clear communication and strategic mindset, I'm confident he will excel in any team dynamic.",
-  //   role: "Frontend Developer",
-  // },
+  {
+    id: 2,
+    name: "Gabrielle Callegari",
+    avatar: "/Images/Calle.jpg",
+    description:
+      "Beyond his technical skills, Ali brings a contagious positive energy to the workplace. His optimistic outlook and passion for his work are evident in every interaction, and he is always willing to lend a helping hand or share his insights. This attitude not only boosts team morale but also fosters a collaborative and supportive environment where everyone can thrive.",
+    role: "Frontend Developer",
+  },
 ];
