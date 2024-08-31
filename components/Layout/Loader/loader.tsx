b58441288facd6a6b6b6226b45e6be2067d14920
ali navidi <ali_navidi24@yahoo.com>
2024-08-31 07:15:25 +0330

    loader's background color changed

--- components/Layout/Loader/loader.tsx
+++ components/Layout/Loader/loader.tsx
@@ -59,17 +59,6 @@ export default function Loader({ onFinish }: { onFinish: any }) {
       initial="visible"
       animate="visible"
       exit="hidden"
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "#111",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
     >
       <AnimatePresence>
         <motion.div
