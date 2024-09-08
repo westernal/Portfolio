5f24b61de4547c5dac7ef528d6622156a1d243cf
ali navidi <ali_navidi24@yahoo.com>
2024-09-08 19:10:16 +0330

    Speed insights added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { montserrat, orbitron } from "../fonts/fonts";
 import "../styles/style.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Layout>
         <>
           <Analytics />
+          <SpeedInsights />
           <Component {...pageProps} />
         </>
       </Layout>
